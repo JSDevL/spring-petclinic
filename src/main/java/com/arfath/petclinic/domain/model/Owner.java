8e3841bc7f6bd42c255654ce0c4550ee4ac8107d
arfath <arfath@niiu.de>
2018-08-20 12:35:27 +0530

    added POJOs

---
+++ src/main/java/com/arfath/petclinic/domain/model/Owner.java
@@ -0,0 +1,4 @@
+package com.arfath.petclinic.domain.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/arfath/petclinic/domain/model/PetType.java
@@ -0,0 +1,13 @@
+package com.arfath.petclinic.domain.model;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
